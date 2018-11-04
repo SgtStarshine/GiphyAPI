eae8d6873895226be10b695102035f2de8ebc11c
SgtStarshine <starshine621@ymail.com>
2018-11-04 00:02:37 -0400

    update

--- script.js
+++ script.js
@@ -56,6 +56,9 @@ $(document).ready( function() {
             // Giving the image tag an src attribute of a proprty pulled off the
             // result item
             topicImage.attr("src", results[j].images.fixed_height_still.url);
+            topicImage.attr("data-still", results[j].images.fixed_height_still.url);
+            topicImage.attr("data-animate", results[j].images.fixed_height.url);
+            topicImage.attr("data-state", "still");
 
             // Appending the paragraph and topicImage we created to the "gifDiv" div we created            
             gifDiv.append(topicImage);
@@ -64,8 +67,23 @@ $(document).ready( function() {
             // Prepending the gifDiv to the "#images" div in the HTML
             $("#images").prepend(gifDiv);
                 
-            }       
+            }
+            
+            $(".image-click").on("click", function() {
+
+                var state = $(this).attr("data-state");
+
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
         });
     });
+
+    
 
 })
