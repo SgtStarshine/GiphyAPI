c2c1c771322c2e4841e462ee30594b0e286d05b7
SgtStarshine <starshine621@ymail.com>
2018-11-03 23:51:47 -0400

    update

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 $(document).ready( function() {
 
-    var topics = ["cats","dogs","birds"];
+    var topicArray = ["cats","dogs","birds"];
 
-    for (var i = 0; i < topics.length; i++) {
+    for (var i = 0; i < topicArray.length; i++) {
 
         var topicsBtn = $("<button>");
 
-        topicsBtn.addClass("topic-button topic topic-button-color");
+        topicsBtn.addClass("topic topic-button-color");
 
-        topicsBtn.attr("topic", topics[i]);
+        topicsBtn.attr("topic", topicArray[i]);
 
-        topicsBtn.text(topics[i]);
+        topicsBtn.text(topicArray[i]);
 
         $("#buttons").append(topicsBtn);
       }  
 
-    // Event listener for our cat-button
-    $(".topic-button").on("click", function() {
+    // Event listener
+    $("button").on("click", function() {
 
-    // Storing our giphy API URL for a random cat image
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=8XD6AWqjbueVXKiGPG33J72nIZNpVGWx";
+    var topic = $(this).attr("topic");
+
+    // Storing our giphy API URL for an image
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
+        topic + "&api_key=8XD6AWqjbueVXKiGPG33J72nIZNpVGWx&limit=10";
 
     // Perfoming an AJAX GET request to our queryURL
     $.ajax({
@@ -28,22 +31,40 @@ $(document).ready( function() {
     })
 
     // After the data from the AJAX request comes back
-        .then(function(response) {
+    .then(function(response) {
 
         console.log(response);
+        
+        var results = response.data;
 
-        // Saving the image_original_url property
-        var imageUrl = response.data.image_original_url;
+        for (var j = 0; j < results.length; j++) {
 
-        // Creating and storing an image tag
-        var catImage = $("<img>");
+            // Creating a div for the gif
+            var gifDiv = $("<div>");
+            gifDiv.addClass("giffy");
 
-        // Setting the catImage src attribute to imageUrl
-        catImage.attr("src", imageUrl);
-        catImage.attr("alt", "cat image");
+            // Storing the result item's rating
+            var rating = results[j].rating;
 
-        // Prepending the catImage to the images div
-        $("#images").prepend(catImage);
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + rating);
+
+            // Creating an image tag
+            var topicImage = $("<img>");
+            topicImage.addClass("image-click");
+
+            // Giving the image tag an src attribute of a proprty pulled off the
+            // result item
+            topicImage.attr("src", results[j].images.fixed_height_still.url);
+
+            // Appending the paragraph and topicImage we created to the "gifDiv" div we created            
+            gifDiv.append(topicImage);
+            gifDiv.append(p);
+
+            // Prepending the gifDiv to the "#images" div in the HTML
+            $("#images").prepend(gifDiv);
+                
+            }       
         });
     });
 
